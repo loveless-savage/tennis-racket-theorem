106fd727df87c0981121e8825088309e5a3f32de
loveless-savage <titansofalgenia@gmail.com>
2024-06-12 23:11:11 -0600

    Plot of basic behavior

--- main.m
+++ main.m
@@ -7,4 +7,8 @@ I1 = 1; I2 = 2; I3 = 3; % test case intertial moment values to see if ODE works
 % define Euler equations
 dwdt = @(t,w) [(I2-I3)/I1*w(2)*w(3);(I3-I1)/I2*w(3)*w(1);(I1-I2)/I1*w(1)*w(2)];
 % solve system of ODEs
-[t,w] = ode45(dwdt,[0 10],[1 0.1 0.1],odeset('RelTol',1e-6));
+[t,w] = ode45(dwdt,[0 30],[0.1 1 0],odeset('RelTol',1e-6));
+
+plot(t,w);
+grid on;
+legend('x','y','z');
