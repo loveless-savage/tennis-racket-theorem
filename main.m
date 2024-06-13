003406cac07de8171ccfb5cb3a22c00707b02823
elons3 <152450688+elons3@users.noreply.github.com>
2024-06-13 17:03:44 -0600

    Animation
    Copied from lab 10, start of tweaks to make it work for tennis racket

--- main.m
+++ main.m
@@ -11,4 +11,33 @@ dwdt = @(t,w) [(I2-I3)/I1*w(2)*w(3);(I3-I1)/I2*w(3)*w(1);(I1-I2)/I1*w(1)*w(2)];
 
 plot(t,w);
 grid on;
-legend('x','y','z');
+legend('\(\omega_x\)','\(\omega_y\)','\(\omega_z\)',Interpreter = 'latex');
+
+%% Animation Station
+te = 0:0.1:30; % evenly spaced time array
+we = interp1(t,w(:,1),te,'spline'); % fix when we know which column of w is theta
+A = max(we);
+
+tau=te(2)-te(1);
+for istep=1:length(te)
+    % Position of swing relative to the pivot
+    theta = we(istep);
+    xswing=l1*sin(theta);
+    yswing=-l1*cos(theta);
+    % position of head/legs with respect to swing
+    phi=A + A*cos(wp*te(istep));
+    xpers=l2*sin(phi + theta);
+    ypers=l2*cos(phi + theta);
+    % Plot the swing and the swinger
+    plot([0, xswing],[L,L+yswing],...
+        [xswing+xpers,xswing-xpers],[L+yswing-ypers,L+yswing+ypers])
+    % Make the x and y dimensions scale equally
+    axis([-L/2 L/2 0 L])
+    axis square
+    % We'd like the plots frames to show at intervals of tau so the movie
+    % matches the physical time scale.  However, the calculations
+    % and plotting take some time, so we decrease the pause a bit.
+    % Depending on the speed of your computer, you may need to adjust
+    % this offset some.
+    pause(tau-0.01)
+ end
